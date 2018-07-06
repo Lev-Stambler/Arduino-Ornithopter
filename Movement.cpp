e8e17664b88beaf1be3042c1a0b451e82efad14e
Lev-Stambler <levstamb@gmail.com>
2018-07-05 20:20:49 -0400

    Update Movement.cpp

--- Movement.cpp
+++ Movement.cpp
@@ -15,13 +15,17 @@ Movement::Movement(int x_pin, int y_pin, int motor_pin)
   pinMode(motor_pin, OUTPUT);
 }
 
-void Movement::Move(int x_angle, int y_angle)
+void Movement::Move_XY(int x_angle, int y_angle)
 {
    //int x_correction = map(x,// , ,0, 180);
    //int y_correction = map(y,// , , 0, 180);
-   
    servo_x.write(x_angle);
    servo_y.write(y_angle);
 }
                          
+void Movement::Move_Z(int power)
+{
+  int power_mapped = map(power, 100, 0, 255);
+  analogWrite(motor_pin, power_mapped);
+}
                          
