6d4d17be2942cd44a8552dee7a02b20c39f12d0e
Lev-Stambler <levstamb@gmail.com>
2018-07-05 20:53:44 -0400

    Rename Orthinopter.ino to Ornithopter.ino

---
+++ Ornithopter.ino
@@ -0,0 +1,15 @@
+#include "Accelerometer.h"
+
+Accelerometer acc;
+
+void setup() {
+  // put your setup code here, to run once:
+  Accelerometer* acc = new Accelerometer();
+  
+}
+
+void loop() 
+{
+  // put your main code here, to run repeatedly:
+    
+}
