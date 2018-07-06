43b6360b80343c490c1ede9b6c222227814fd5ef
Lev-Stambler <levstamb@gmail.com>
2018-07-05 20:08:09 -0400

    Update Movement.cpp

--- Movement.cpp
+++ Movement.cpp
@@ -5,10 +5,7 @@
 
 Servo servo_x;
 Servo servo_y;
-int x = //acc value
-int y = //acc value
-int x_correction = map(x,// , ,0, 180);
-int y_correction = map(y,// , , 0, 180);
+
 
 
 Movement::Movement(int x_pin, int y_pin, int motor_pin)
@@ -20,6 +17,9 @@ Movement::Movement(int x_pin, int y_pin, int motor_pin)
 
 void Movement::Move(int x_angle, int y_angle)
 {
+   //int x_correction = map(x,// , ,0, 180);
+   //int y_correction = map(y,// , , 0, 180);
+   
    servo_x.write(x_angle);
    servo_y.write(y_angle);
 }
