a0acfc9f33bb55678f6a56ded45fdd9e3a74f15b
Lev-Stambler <levstamb@gmail.com>
2018-07-05 20:06:58 -0400

    Update Movement.cpp

--- Movement.cpp
+++ Movement.cpp
@@ -1,7 +1,27 @@
 #include "Arduino.h"
 #include "Movement.h"
-Movement::Movement()
+
+#include <Servo.h>
+
+Servo servo_x;
+Servo servo_y;
+int x = //acc value
+int y = //acc value
+int x_correction = map(x,// , ,0, 180);
+int y_correction = map(y,// , , 0, 180);
+
+
+Movement::Movement(int x_pin, int y_pin, int motor_pin)
 {
-  ///void setup
+  servo_x.attach(x_pin); 
+  servo_y.attach(y_pin);
+  pinMode(motor_pin, OUTPUT);
 }
 
+void Movement::Move(int x_angle, int y_angle)
+{
+   servo_x.write(x_angle);
+   servo_y.write(y_angle);
+}
+                         
+                         
