9341bff8d3ab3e0ed5eb05adbf5e1b193d16a91f
2nina2 <37350752+2nina2@users.noreply.github.com>
2018-07-06 21:02:05 -0400

    Create receiver.h

---
+++ receiver.h
@@ -0,0 +1,16 @@
+#ifindef receiver_h
+#define receiver_h
+
+class receiver
+{
+  public:
+  receiver();
+  void SETUP();
+  int[] getMovementCommands();
+  //put setter or whatever we do with the data received here
+  
+  //
+};
+
+extern receiver recevr;
+#endif
