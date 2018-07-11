4c9d5a868fd2cf5062a78a9f6aa9e1c89a0d1545
Lev-Stambler <levstamb@gmail.com>
2018-07-10 21:51:30 -0400

    Update Movement.cpp

--- Ornithopter/Movement.cpp
+++ Ornithopter/Movement.cpp
@@ -9,6 +9,9 @@ Servo servo_Y;
 
 int motor_pin;
 
+/* 
+* This class is used to move the servos and motor
+*/
 
 Movement::Movement()
 {
@@ -23,19 +26,30 @@ void Movement::SETUP(int X_pin, int Y_pin, int Motor_Pin)
 
 }
 
+/*
+* controll movement in the x-direction via servo motor
+*/
+
 void Movement::Move_X(int x_angle)
 {
    servo_X.write(x_angle);
-}//controll movement in the x-direction via servo motor
+}
+
+/*
+* controll movement in the y-direction via servo motor
+*/
 
 void Movement::Move_Y(int y_angle)
 {
   servo_Y.write(y_angle);
-}//controll movement in the y direction via servo motor
-                         
+}
+
+/*
+*controll movement in the z direction vai pulse with modulation (increase or decrease power being sent to wings)
+*/
+
 void Movement::Move_Z(int power)
 {
   int power_mapped = map(power,0, 100, 0, 255);
   analogWrite(motor_pin, power_mapped);
-}//controll movement in the z direction vai pulse with modulation (increase or decrease power being sent to wings)
-                         
+}                       
