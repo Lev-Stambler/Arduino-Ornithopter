cfa369f1ad9a62df15835dfc306098b3af81a0cd
2nina2 <37350752+2nina2@users.noreply.github.com>
2018-07-17 21:07:11 -0400

    Update Plotter.cpp

--- Ornithopter/Plotter.cpp
+++ Ornithopter/Plotter.cpp
@@ -4,20 +4,49 @@
 #include "Plotter.h"
 #include "Arduino.h"
 
+const int accelReadings = 10; 
+    int accelX[accelReadings];      
+    int accelY[accelReadings];
+
+
 Plotter :: Plotter(){
   //plot out the values from the accelerometer and servos 
-  
+  currentAccel = 0;              
+  totalAccelX = 0;                 
+  averageAccelX = 0;         
+  //int current.accelY = 0;
+  totalAccelY = 0;
+  averageAccelY = 0;
 }
 
 void Plotter:: SETUP(){
+    for (int thisReading = 0; thisReading < accelReadings; thisReading++) accelX[thisReading] = 0;
+    for (int thisReading = 0; thisReading < accelReadings; thisReading++) accelY[thisReading] = 0;//you could do this in one for loop if you wanted to (maybe)
+}
+
+void Plotter::Smooth(){
+  // subtract the last reading:
+  totalAccelX = totalAccelX - accelX[currentAccel];
+  totalAccelY = totalAccelY - accelY[currentAccel];
+  accelX[currentAccel] = map(Accel.X_Acc, 0, 4000, 0, 180);
+  accelY[currentAccel] = map(Accel.Y_Acc, 0, 4000, 0, 180);
+  totalAccelX = totalAccelX + accelX[currentAccel];
+  totalAccelY = totalAccelY + accelY[currentAccel];
+  // advance to the next position in the array:
+  currentAccel = currentAccel + 1;
+  // if we're at the end of the array...
+  if (currentAccel >= accelReadings && currentAccel >= accelReadings) currentAccel = 0;//you probably don't need && here
+  averageAccelX = totalAccelX / accelReadings;
+  averageAccelY = totalAccelY / accelReadings; 
   
 }
 
 void Plotter::Printer()
 {
-  Serial.print(map(Accel.X_Acc, 0, 4000, 0, 180));
+  Smooth();
+  Serial.print(averageAccelX);
   Serial.print(" "); 
-  Serial.print(map(Accel.Y_Acc, 0, 4000, 0 ,180)); 
+  Serial.print(averageAccelY); 
   Serial.print (" "); 
   Serial.print(calculator.X_Angle); 
   Serial.print(" ");
@@ -25,6 +54,3 @@ void Plotter::Printer()
 }
 
 Plotter Plot = Plotter();
-  
-  
-
