90671ee038d41c4f925c3757f7fecf2ea4e3cf47
Lev-Stambler <levstamb@gmail.com>
2018-07-17 22:03:47 -0400

    Update Transmitter_2.0.ino

--- Transmitter_2.0/Transmitter_2.0.ino
+++ Transmitter_2.0/Transmitter_2.0.ino
@@ -8,7 +8,6 @@ int buttonPin = 2;
 int buttonPrev;
 
 #define NODE_ID          1
-//#define OUTPUT_PIN       11
 
 int X_val, Y_val, Z_val;
 int X_form = 0, Y_form = 0, Z_form = 0;
@@ -23,12 +22,16 @@ RH_ASK transmitter;
 
 void setup() 
 {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(buttonPin, INPUT_PULLUP);
  if (!transmitter.init())
          Serial.println("init failed");
 }
+
+/* 
+* The function of void loop is to send information over the RF Transmitter when the values of the X or Y Joystick, 
+* or Z Potentiometer are changed. When the button on the joystick is pressed, the transmitter sends information as well
+*/
 
 void loop() 
 {
@@ -52,11 +55,15 @@ void loop()
 
   if(abs(X_val - X_form) > 3 || abs(Y_val - Y_form) > 3 || abs(Z_val - Z_form) > 3 || is_Hover_Prev != is_Hovering)
   {
-    String msg_str = String(X_val) + "," + String(Y_val) + "," + String(Z_val) + "," + String(is_Hovering);
-    Serial.println(msg_str);
+    //String msg_str = String(X_val) + "," + String(Y_val) + "," + String(Z_val) + "," + String(is_Hovering);
+    //Serial.println(msg_str);
 
+    /*
+    * The information is sent over in 4 bytes. One for X angle value (0-180), one for Y angle value (0-180), one for Z PWM Power (0-255),
+    * One for hovering state (-1, 1 which goes to 255, 1 due to loopback in uint_8)
+    */
     uint8_t *umsg = new uint8_t[4];
-    char *msg = new char[4];
+
     umsg[0] = map(X_val, 0, 1023, 0, 180);
     umsg[1] = map(Y_val, 0, 1023, 0, 180);
     umsg[2] = map(Z_val, 0, 1023, 0, 255);
@@ -66,7 +73,7 @@ void loop()
     {
       Serial.println(umsg[1]);
       
-      transmitter.send(umsg, 4);//strlen(umsg));
+      transmitter.send(umsg, 4);
       
       transmitter.waitPacketSent();
       delay(50);
@@ -76,6 +83,4 @@ void loop()
     Z_form = Z_val;
     is_Hover_Prev = is_Hovering;
   }
-  
-  
 }
