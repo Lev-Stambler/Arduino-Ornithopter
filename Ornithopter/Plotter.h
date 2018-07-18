8cf9353aa3459db2795251faf5be167eb2480c0c
2nina2 <37350752+2nina2@users.noreply.github.com>
2018-07-17 21:07:44 -0400

    Update Plotter.h

--- Ornithopter/Plotter.h
+++ Ornithopter/Plotter.h
@@ -6,7 +6,15 @@ class Plotter
   public:
     Plotter();
     void SETUP();
+    void Smooth();
     void Printer();
+    int currentAccel,              
+    totalAccelX,                 
+    averageAccelX;      
+    //int current.accelY;
+    int totalAccelY,
+    averageAccelY;
+
 };
 
 extern Plotter Plot;
