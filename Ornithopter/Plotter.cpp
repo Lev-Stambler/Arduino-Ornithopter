f85fa25b475559a6abdf3bff63c29fd524c39f03
Lev-Stambler <levstamb@gmail.com>
2018-07-18 15:07:30 -0400

    Update Plotter.cpp

--- Ornithopter/Plotter.cpp
+++ Ornithopter/Plotter.cpp
@@ -27,8 +27,9 @@ void Plotter::Smooth(){
   // subtract the last reading:
   totalAccelX = totalAccelX - accelX[currentAccel];
   totalAccelY = totalAccelY - accelY[currentAccel];
-  accelX[currentAccel] = map(Accel.X_Acc, 0, 4000, 0, 180);
-  accelY[currentAccel] = map(Accel.Y_Acc, 0, 4000, 0, 180);
+  // acceleration is a scale of 0 to 65536: 16 bits
+  accelX[currentAccel] = map(Accel.X_Acc, 0, 65535, 0, 180);
+  accelY[currentAccel] = map(Accel.Y_Acc, 0, 65535, 0, 180);
   totalAccelX = totalAccelX + accelX[currentAccel];
   totalAccelY = totalAccelY + accelY[currentAccel];
   // advance to the next position in the array:
