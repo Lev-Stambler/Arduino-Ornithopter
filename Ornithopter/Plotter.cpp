7e05e7352caf3f112aca42aae5f51f4528441b5f
2nina2 <37350752+2nina2@users.noreply.github.com>
2018-07-20 18:26:50 -0400

    Update Plotter.cpp

--- Ornithopter/Plotter.cpp
+++ Ornithopter/Plotter.cpp
@@ -6,6 +6,7 @@
 const int accelReadings = 10; 
     int accelX[accelReadings];      
     int accelY[accelReadings];
+    int accelZ[accelReadings];
 
 
 Plotter :: Plotter(){
@@ -13,31 +14,36 @@ Plotter :: Plotter(){
   currentAccel = 0;              
   totalAccelX = 0;                 
   averageAccelX = 0;         
-  //int current.accelY = 0;
   totalAccelY = 0;
   averageAccelY = 0;
+  totalAccelZ = 0;
+  averageAccelZ=0;
 }
 
 void Plotter:: SETUP(){
     for (int i = 0; i < accelReadings; i++) accelX[i] = 0;
     for (int i = 0; i < accelReadings; i++) accelY[i] = 0;
+    for (int i = 0; i < accelReadings; i++) accelZ[i] = 0;
 }
 
 void Plotter::Smooth(){
   // subtract the last reading:
   totalAccelX = totalAccelX - accelX[currentAccel];
   totalAccelY = totalAccelY - accelY[currentAccel];
+  totalAccelZ = totalAccelZ - accelZ[currentAccel];
   // acceleration is a scale of 0 to 65536: 16 bits
   accelX[currentAccel] = map(Accel.X_Acc, -32768, 32768, -19.6, 19.6); //on a range of -2g to 2g (9.8m/s2)
   accelY[currentAccel] = map(Accel.Y_Acc, -32768, 32768, -19.6, 19.6); //on a range of -2g to 2g (9.8m/s2)
+  accelZ[currentAccel] = map(Accel.Z_Acc, -32768, 32768, -19.6, 19.6);
   totalAccelX = totalAccelX + accelX[currentAccel];
   totalAccelY = totalAccelY + accelY[currentAccel];
+  totalAccelZ = totalAccelZ + accelZ[currentAccel];
   // advance to the next position in the array:
   currentAccel = currentAccel + 1;
-  if (currentAccel >= accelReadings && currentAccel >= accelReadings) currentAccel = 0;//you probably don't need && here
+  if (currentAccel >= accelReadings) currentAccel = 0;//you probably don't need && here
   averageAccelX = totalAccelX / accelReadings;
   averageAccelY = totalAccelY / accelReadings; 
-  
+  averageAccelZ = totalAccelZ / accelReadings;
 }
 
 void Plotter::Printer()
@@ -46,10 +52,14 @@ void Plotter::Printer()
   Serial.print(averageAccelX);
   Serial.print(" "); 
   Serial.print(averageAccelY); 
-  Serial.print (" "); 
+  Serial.print(" "); 
+  Serial.print(averageAccelZ);
+  Serial.print(" ");
   Serial.print(calculator.X_Angle); 
   Serial.print(" ");
-  Serial.println(calculator.Y_Angle);
+  Serial.print(calculator.Y_Angle);
+  Serial.print(" ");
+  Serial.println(calculator.Z_Power);
 }
 
 Plotter Plot = Plotter();
