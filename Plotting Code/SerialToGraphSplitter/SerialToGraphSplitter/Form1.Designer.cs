8c0b3a887e23fcc3477ae975c8032c2c4f1206de
Lev-Stambler <levstamb@gmail.com>
2018-07-21 17:57:20 -0400

    Add files via upload

---
+++ Plotting Code/SerialToGraphSplitter/SerialToGraphSplitter/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace SerialToGraphSplitter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBoxInital = new System.Windows.Forms.RichTextBox();
+            this.textXAcc = new System.Windows.Forms.RichTextBox();
+            this.textYAcc = new System.Windows.Forms.RichTextBox();
+            this.textAccZ = new System.Windows.Forms.RichTextBox();
+            this.textXServo = new System.Windows.Forms.RichTextBox();
+            this.textYServo = new System.Windows.Forms.RichTextBox();
+            this.textZPower = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textTime = new System.Windows.Forms.RichTextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBoxInital
+            // 
+            this.richTextBoxInital.Location = new System.Drawing.Point(12, 12);
+            this.richTextBoxInital.Name = "richTextBoxInital";
+            this.richTextBoxInital.Size = new System.Drawing.Size(799, 231);
+            this.richTextBoxInital.TabIndex = 0;
+            this.richTextBoxInital.Text = "";
+            // 
+            // textXAcc
+            // 
+            this.textXAcc.Location = new System.Drawing.Point(1035, 12);
+            this.textXAcc.Name = "textXAcc";
+            this.textXAcc.Size = new System.Drawing.Size(799, 133);
+            this.textXAcc.TabIndex = 1;
+            this.textXAcc.Text = "";
+            // 
+            // textYAcc
+            // 
+            this.textYAcc.Location = new System.Drawing.Point(1035, 196);
+            this.textYAcc.Name = "textYAcc";
+            this.textYAcc.Size = new System.Drawing.Size(799, 133);
+            this.textYAcc.TabIndex = 2;
+            this.textYAcc.Text = "";
+            // 
+            // textAccZ
+            // 
+            this.textAccZ.Location = new System.Drawing.Point(1035, 369);
+            this.textAccZ.Name = "textAccZ";
+            this.textAccZ.Size = new System.Drawing.Size(799, 133);
+            this.textAccZ.TabIndex = 3;
+            this.textAccZ.Text = "";
+            // 
+            // textXServo
+            // 
+            this.textXServo.AcceptsTab = true;
+            this.textXServo.Location = new System.Drawing.Point(1035, 548);
+            this.textXServo.Name = "textXServo";
+            this.textXServo.Size = new System.Drawing.Size(799, 133);
+            this.textXServo.TabIndex = 4;
+            this.textXServo.Text = "";
+            // 
+            // textYServo
+            // 
+            this.textYServo.AcceptsTab = true;
+            this.textYServo.Location = new System.Drawing.Point(1035, 710);
+            this.textYServo.Name = "textYServo";
+            this.textYServo.Size = new System.Drawing.Size(799, 133);
+            this.textYServo.TabIndex = 5;
+            this.textYServo.Text = "";
+            // 
+            // textZPower
+            // 
+            this.textZPower.AcceptsTab = true;
+            this.textZPower.Location = new System.Drawing.Point(1035, 887);
+            this.textZPower.Name = "textZPower";
+            this.textZPower.Size = new System.Drawing.Size(799, 133);
+            this.textZPower.TabIndex = 6;
+            this.textZPower.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 255);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 29);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Input";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(954, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 29);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "X Acc";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(954, 300);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 29);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Y Acc";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(954, 473);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 29);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Z Acc";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(930, 652);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(99, 29);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "X Servo";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(931, 814);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(98, 29);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Y Servo";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(933, 991);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(96, 29);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Z Servo";
+            // 
+            // textTime
+            // 
+            this.textTime.Location = new System.Drawing.Point(12, 680);
+            this.textTime.Name = "textTime";
+            this.textTime.Size = new System.Drawing.Size(799, 367);
+            this.textTime.TabIndex = 14;
+            this.textTime.Text = "";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 648);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(69, 29);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Time";
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(256, 382);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(255, 228);
+            this.buttonStart.TabIndex = 16;
+            this.buttonStart.Text = "Begin Convo";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1846, 1081);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textTime);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textZPower);
+            this.Controls.Add(this.textYServo);
+            this.Controls.Add(this.textXServo);
+            this.Controls.Add(this.textAccZ);
+            this.Controls.Add(this.textYAcc);
+            this.Controls.Add(this.textXAcc);
+            this.Controls.Add(this.richTextBoxInital);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(1874, 1160);
+            this.MinimumSize = new System.Drawing.Size(1874, 1160);
+            this.Name = "Form1";
+            this.Text = "Text Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBoxInital;
+        private System.Windows.Forms.RichTextBox textXAcc;
+        private System.Windows.Forms.RichTextBox textYAcc;
+        private System.Windows.Forms.RichTextBox textAccZ;
+        private System.Windows.Forms.RichTextBox textXServo;
+        private System.Windows.Forms.RichTextBox textYServo;
+        private System.Windows.Forms.RichTextBox textZPower;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.RichTextBox textTime;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button buttonStart;
+    }
+}
+
---
+++ Plotting Code/SerialToGraphSplitter/SerialToGraphSplitter/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace SerialToGraphSplitter
+{
+    public partial class Form1 : Form
+    {
+        private SerialPort port = new SerialPort();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ClosePort()
+        {
+            Thread.Sleep(5000);
+            port.Close();
+            while (true) ;
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            textTime.Text += "";
+            textXAcc.Text += "";
+            textYAcc.Text += "";
+            textAccZ.Text += "";
+            textZPower.Text += "";
+            textXServo.Text += "";
+            textYServo.Text += "";
+            int initalTime = 0;
+            port.PortName = SerialPort.GetPortNames()[0];
+            port.BaudRate = 57600;
+            port.Parity = Parity.None;
+            //port.StopBits = StopBits.None;
+            port.Open();
+            Thread stopper = new Thread(ClosePort);
+            //stopper.Start();
+            string rawInput = "";
+            Console.WriteLine("OPEENED");
+            bool going = true;
+            long initial = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+
+            while (DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond - initial < 8000) 
+            {
+                rawInput += Convert.ToChar(port.ReadChar()).ToString();
+                going = port.IsOpen;
+            }
+            port.Close();
+            //string rawInput = richTextBoxInital.Text;
+            string[] largeArray = rawInput.Split('^');
+            // all the -1 is because there is a null addition of ^ at the end
+            string[] time = new string[largeArray.Length - 1];
+            string[] XAcc = new string[largeArray.Length - 1];
+            string[] YAcc = new string[largeArray.Length - 1];
+            string[] ZAcc = new string[largeArray.Length - 1];
+            string[] XServo = new string[largeArray.Length - 1];
+            string[] YServo = new string[largeArray.Length - 1];
+            string[] ZPower = new string[largeArray.Length - 1];
+
+            for(int i = 3; i < largeArray.Length - 1; i++)
+            {
+                if (i == 3)
+                {
+                    textTime.Text += "[";
+                    textXAcc.Text += "[";
+                    textYAcc.Text += "[";
+                    textAccZ.Text += "[";
+                    textZPower.Text += "[";
+                    textXServo.Text += "[";
+                    textYServo.Text += "[";
+                    initalTime = Convert.ToInt32(largeArray[i].Split('|')[0]);
+                }
+
+                string[] splitted = largeArray[i].Split('|');
+                time[i] = (Convert.ToInt32(splitted[0]) - initalTime).ToString();
+                XAcc[i] = splitted[1];
+                YAcc[i] = splitted[2];
+                ZAcc[i] = splitted[3];
+                XServo[i] = splitted[4];
+                YServo[i] = splitted[5];
+                ZPower[i] = splitted[6];
+
+                
+                textTime.Text += time[i];
+                textXAcc.Text += XAcc[i];
+                textYAcc.Text += YAcc[i];
+                textAccZ.Text += ZAcc[i];
+                textZPower.Text += ZPower[i];
+                textXServo.Text += XServo[i];
+                textYServo.Text += YServo[i];
+
+                if(i < largeArray.Length - 2)
+                {
+                    textTime.Text += ",";
+                    textXAcc.Text += ",";
+                    textYAcc.Text += ",";
+                    textAccZ.Text += ",";
+                    textZPower.Text += ",";
+                    textXServo.Text += ",";
+                    textYServo.Text += ",";
+                }
+
+
+            }
+            textTime.Text += "]";
+            textXAcc.Text += "]";
+            textYAcc.Text += "]";
+            textAccZ.Text += "]";
+            textZPower.Text += "]";
+            textXServo.Text += "]";
+            textYServo.Text += "]";
+        }
+    }
+}
